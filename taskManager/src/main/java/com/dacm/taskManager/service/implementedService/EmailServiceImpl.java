edad989c3eb070d96ca83732d9571a1231b41776
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-03 17:14:49 +0100

    UPDATED EMAIL SERVICE

--- taskManager/src/main/java/com/dacm/taskManager/service/implementedService/EmailServiceImpl.java
+++ taskManager/src/main/java/com/dacm/taskManager/service/implementedService/EmailServiceImpl.java
@@ -1,9 +1,12 @@
 package com.dacm.taskManager.service.implementedService;
 
 import com.dacm.taskManager.service.interfaceService.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,16 +15,28 @@ public class EmailServiceImpl implements EmailService {
 
 
     public final JavaMailSender sendMessage;
+    private SimpleMailMessage message;
+
     @Override
     public void sendPasswordResetEmail(String toEmail, String newPassword) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        message = new SimpleMailMessage();
         message.setTo(toEmail);
-        message.setFrom("example@mail.com");
-        message.setSubject("Password Reset");
-        message.setText("Your new password is: " + newPassword + "\n" +
+        message.setFrom("@example.com");
+        message.setSubject("Password reset");
+        message.setText("Your new passwsford is: " + newPassword + "\n" +
                 "Don't reply this message");
 
+        sendMessage.send(message);
+    }
+
+    @Override
+    public void sendEmail(String toEmail, String subject, String text) {
+        message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setFrom("@example.com");
+        message.setSubject(subject);
+        message.setText(text);
         sendMessage.send(message);
     }
 }
